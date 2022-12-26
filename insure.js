4d563c769d9e351101a8bbe4038433f469d54d43
ogpeprah1 <102240482+ogpeprah1@users.noreply.github.com>
2022-12-26 08:17:13 +0000

    Made the side bar transiton more flexible

--- insure.js
+++ insure.js
@@ -5,11 +5,11 @@ const menuItems = document.querySelector('.nav-item')
 menuOpen.addEventListener('click', function(){
     menuOpen.style.display = 'none';
     menuClosed.style.display = 'block';
-    menuItems.style.display = 'flex';
+    menuItems.style.top = '100%';
 })
 
 menuClosed.addEventListener('click', function(){
     menuOpen.style.display = 'block';
     menuClosed.style.display = 'none';
-    menuItems.style.display = 'none';
+    menuItems.style.top = '-100vh';
 })
